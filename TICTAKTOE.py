644fea0111024d222155d8a27ccb41fed1c59080
Vuk Milikic <vuk.milikic@3ap.ch>
2024-09-09 11:18:50 +0200

    erledigtersterteil

--- TICTAKTOE.py
+++ TICTAKTOE.py
@@ -22,3 +22,26 @@ def modifyArray(number, player):
         gameBoard[row][col] = player
     else:
         print("This spot is already taken. Try again.")
+
+
+def checkWinner():
+    
+    for row in gameBoard:
+        if row[0] == row[1] == row[2]: 
+            return row[0]
+
+    
+    for col in range(3):
+        if gameBoard[0][col] == gameBoard[1][col] == gameBoard[2][col]:
+            return gameBoard[0][col]
+
+    
+    if gameBoard[0][0] == gameBoard[1][1] == gameBoard[2][2]:
+        return gameBoard[0][0]
+
+   
+    if gameBoard[0][2] == gameBoard[1][1] == gameBoard[2][0]:
+        return gameBoard[0][2]
+
+    
+    return None
