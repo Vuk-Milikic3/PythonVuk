d13d8329583576a9730ccb46530dd4a75eb48727
vukmilikic3 <vuk.milikic@3ap.ch>
2024-09-09 09:19:29 +0200

    inausfürung

--- TICTAKTOE.py
+++ TICTAKTOE.py
@@ -1,69 +1,42 @@
 import random
 
-def print_board(board):
-    """Druckt das aktuelle Spielfeld."""
-    for row in [board[i:i+3] for i in range(0, len(board), 3)]:
-        print(" | ".join(row))
-        if board.index(row[0]) < 6:
-            print("--+---+--")
+print("Welcome to Tic Tac Toe")
+print("----------------------")
 
-def check_win(board, player):
-    """Überprüft, ob der angegebene Spieler gewonnen hat."""
-    win_conditions = [(0, 1, 2), (3, 4, 5), (6, 7, 8), # Horizontale Gewinnmöglichkeiten
-                      (0, 3, 6), (1, 4, 7), (2, 5, 8), # Vertikale Gewinnmöglichkeiten
-                      (0, 4, 8), (2, 4, 6)]            # Diagonale Gewinnmöglichkeiten
-    
-    for condition in win_conditions:
-        if board[condition[0]] == board[condition[1]] == board[condition[2]] == player:
-            return True
-    return False
+possibleNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+gameBoard = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rows = 3
+cols = 3
 
-def check_tie(board):
-    """Überprüft, ob es ein Unentschieden gibt."""
-    return all([spot in ['x', 'o'] for spot in board])
+def printGameBoard():
+    for x in range(rows):
+        print("\n+---+---+---+")
+        print("|", end="")
+        for y in range(cols):
+            print("", gameBoard[x][y], end=" |")
+    print("\n+---+---+---+")
 
-def player_move(board, player):
-    """Lässt den Spieler einen Zug machen."""
-    move = int(input("Wähle dein Feld aus (1-9): ")) - 1
-    if board[move] not in ['x', 'o']:
-        board[move] = player
-    else:
-        print("Feld bereits belegt. Versuche es erneut.")
-        player_move(board, player)
+def modifyArray(number, turn):
+    number -= 1  
+    if number == 0:
+        gameBoard[0][0] = turn
+    elif number == 1:
+        gameBoard[0][1] = turn
+    elif number == 2:
+        gameBoard[0][2] = turn
+    elif number == 3:
+        gameBoard[1][0] = turn
+    elif number == 4:
+        gameBoard[1][1] = turn
+    elif number == 5:
+        gameBoard[1][2] = turn
+    elif number == 6:
+        gameBoard[2][0] = turn
+    elif number == 7:
+        gameBoard[2][1] = turn
+    elif number == 8:
+        gameBoard[2][2] = turn
 
-def ai_move(board):
-    """KI macht einen zufälligen Zug."""
-    available_moves = [i for i in range(9) if board[i] not in ['x', 'o']]
-    move = random.choice(available_moves)
-    board[move] = 'o'
+leaveLoop = False
+turnCounter = 0
 
-def tic_tac_toe():
-    """Hauptfunktion für das Tic-Tac-Toe-Spiel."""
-    board = [str(i+1) for i in range(9)]
-    current_player = 'x'
-    game_over = False
-    
-    while not game_over:
-        print_board(board)
-        if current_player == 'x':
-            player_move(board, current_player)
-        else:
-            ai_move(board)
-        
-        if check_win(board, current_player):
-            print_board(board)
-            print(f"Spieler {current_player} hat das Spiel gewonnen.")
-            game_over = True
-        elif check_tie(board):
-            print_board(board)
-            print("Unentschieden!")
-            game_over = True
-        else:
-            current_player = 'o' if current_player == 'x' else 'x'
-    
-    restart = input("Möchtest du ein weiteres Spiel spielen? (y/n): ")
-    if restart.lower() == 'y':
-        tic_tac_toe()
-
-# Spiel starten
-tic_tac_toe()
