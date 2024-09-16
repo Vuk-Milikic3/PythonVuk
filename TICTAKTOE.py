deccb038c7dd3c44ee0cc2082ca73f3661dc9ee8
Vuk Milikic <vuk.milikic@3ap.ch>
2024-09-16 16:40:21 +0200

    vebesserug

--- TICTAKTOE.py
+++ TICTAKTOE.py
@@ -3,144 +3,123 @@ import random
 print("Welcome to Tic Tac Toe")
 print("----------------------")
 
-def resetGameBoard():
-    return [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+player1_symbol = 'X'
+player2_symbol = 'O'
 
-gameBoard = resetGameBoard()
+def create_new_board():
+    return [[1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]]
 
-def printGameBoard():
-
+def game_board(board):
     print("+---+---+---+")
-    print(f"| {gameBoard[0][0]} | {gameBoard[0][1]} | {gameBoard[0][2]} |")
-    print("+---+---+---+")
-    print(f"| {gameBoard[1][0]} | {gameBoard[1][1]} | {gameBoard[1][2]} |")
-    print("+---+---+---+")
-    print(f"| {gameBoard[2][0]} | {gameBoard[2][1]} | {gameBoard[2][2]} |")
-    print("+---+---+---+")
+    for row in board:
+        print(f"| {' | '.join(str(cell) for cell in row)} |")
+        print("+---+---+---+")
 
-def modifyArray(number, player):
+def get_player_move(player_symbol):
+    while True:
+        try:
+            move = int(input(f"Player {player_symbol}, choose a field (1-9): "))
+            if 1 <= move <= 9:
+                return move
+            else:
+                print("Invalid input. Please enter a number between 1 and 9.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 1 and 9.")
 
-    if number == 1:
-        if gameBoard[0][0] != "X" and gameBoard[0][0] != "O":
-            gameBoard[0][0] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 2:
-        if gameBoard[0][1] != "X" and gameBoard[0][1] != "O":
-            gameBoard[0][1] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 3:
-        if gameBoard[0][2] != "X" and gameBoard[0][2] != "O":
-            gameBoard[0][2] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 4:
-        if gameBoard[1][0] != "X" and gameBoard[1][0] != "O":
-            gameBoard[1][0] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 5:
-        if gameBoard[1][1] != "X" and gameBoard[1][1] != "O":
-            gameBoard[1][1] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 6:
-        if gameBoard[1][2] != "X" and gameBoard[1][2] != "O":
-            gameBoard[1][2] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 7:
-        if gameBoard[2][0] != "X" and gameBoard[2][0] != "O":
-            gameBoard[2][0] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 8:
-        if gameBoard[2][1] != "X" and gameBoard[2][1] != "O":
-            gameBoard[2][1] = player
-        else:
-            print("This spot is already taken. Try again.")
-    elif number == 9:
-        if gameBoard[2][2] != "X" and gameBoard[2][2] != "O":
-            gameBoard[2][2] = player
-        else:
-            print("This spot is already taken. Try again.")
+def update_board(board, move, player_symbol):
+    field_coordinates = {
+        1: (0, 0),
+        2: (0, 1),
+        3: (0, 2),
+        4: (1, 0),
+        5: (1, 1),
+        6: (1, 2),
+        7: (2, 0),
+        8: (2, 1),
+        9: (2, 2)
+    }
+    row, col = field_coordinates[move]
+    if board[row][col] not in [player1_symbol, player2_symbol]:
+        board[row][col] = player_symbol
+        return True
     else:
-        print("Invalid input, please enter a number between 1 and 9.")
+        print("This field is already taken. Please choose another field.")
+        return False
 
-def checkWinner():
+def check_for_winner(board):
 
-    if gameBoard[0][0] == gameBoard[0][1] == gameBoard[0][2]:
-        return gameBoard[0][0]
-    elif gameBoard[1][0] == gameBoard[1][1] == gameBoard[1][2]:
-        return gameBoard[1][0]
-    elif gameBoard[2][0] == gameBoard[2][1] == gameBoard[2][2]:
-        return gameBoard[2][0]
+    for row in board:
+        if row[0] == row[1] == row[2]:
+            return row[0]
     
-    if gameBoard[0][0] == gameBoard[1][0] == gameBoard[2][0]:
-        return gameBoard[0][0]
-    elif gameBoard[0][1] == gameBoard[1][1] == gameBoard[2][1]:
-        return gameBoard[0][1]
-    elif gameBoard[0][2] == gameBoard[1][2] == gameBoard[2][2]:
-        return gameBoard[0][2]
-
-    if gameBoard[0][0] == gameBoard[1][1] == gameBoard[2][2]:
-        return gameBoard[0][0]
-    elif gameBoard[0][2] == gameBoard[1][1] == gameBoard[2][0]:
-        return gameBoard[0][2]
-
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col]:
+            return board[0][col]
+    
+    if board[0][0] == board[1][1] == board[2][2]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0]:
+        return board[0][2]
+    
     return None
 
-def playGame():
-
-    currentPlayer = "X"
-    moves = 0
-
-    while moves < 9:
-        printGameBoard()
-
-        try:
-            move = int(input(f"Player {currentPlayer}, choose a number (1-9): "))
-        except ValueError:
-            print("Please enter a valid number between 1 and 9.")
-            continue
-
-        if move < 1 or move > 9:
-            print("Invalid move. Please try again.")
-            continue
-
-        modifyArray(move, currentPlayer)
-
-        winner = checkWinner()
-        if winner:
-            printGameBoard()
-            print(f"Player {winner} wins!")
-            return
-
-        if currentPlayer == "X":
-            currentPlayer = "O"
-        else:
-            currentPlayer = "X"
-
-        moves += 1
-
-    printGameBoard()
-    print("It's a tie!")
-
-def askForAnotherGame():
+def ask_to_play_again():
     while True:
         answer = input("Do you want to play another game? (yes/no): ").lower()
-        if answer == "yes" or answer == "y":
+        if answer in ["yes", "y"]:
             return True
-        elif answer == "no" or answer == "n":
+        elif answer in ["no", "n"]:
             return False
         else:
             print("Please answer with 'yes' or 'no'.")
 
+def select_game_mode():
+    while True:
+        mode = input("Do you want to play against the computer or another player? (computer/player): ").lower()
+        if mode in ["player", "p"]:
+            return "player"
+        elif mode in ["computer", "c"]:
+            print("Computer opponent is not implemented yet.")
+            return None
+        else:
+            print("Please answer with 'computer' or 'player'.")
+
+def play_game():
+    board = create_new_board()
+    game_mode = select_game_mode()
+    if game_mode != "player":
+        return
+
+
+
+    current_player_symbol = player1_symbol
+
+    for total_moves in range(9):
+        game_board(board)
+        
+        # Get player move
+        move = get_player_move(current_player_symbol)
+        
+        if not update_board(board, move, current_player_symbol):
+            continue
+        
+        winner = check_for_winner(board)
+        if winner:
+            game_board(board)
+            print(f"{winner} wins!")
+            return
+        
+        current_player_symbol = player2_symbol if current_player_symbol == player1_symbol else player1_symbol
+        
+
+    game_board(board)
+    print("It's a tie!")
+
+
 while True:
-    playGame()
-    if not askForAnotherGame():
+    play_game()
+    if not ask_to_play_again():
         print("Thanks for playing! Goodbye!")
         break
-    else:
-        gameBoard = resetGameBoard()
